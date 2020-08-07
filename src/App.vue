6bfb03cdc76798b5004e9f835eda351af7446056
kotarofunyu <kotarofunyu@gmail.com>
2020-08-07 20:05:51 +0900

    vuetify導入

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,60 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link to="/page1">Page1</router-link>
-      <router-link to="/page2">Page2</router-link>
-      <router-link to="/page3">Page3</router-link>
-      <router-link to="/signIn" v-if="!signedIn">ログイン</router-link>
-      <router-link to="mypage" v-if="signedIn">マイページ</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
-
 <script>
-  import { mapState } from 'vuex'
+import HelloWorld from './components/HelloWorld';
 
-  export default {
-    name: 'Header',
-    computed: mapState([
-      'signedIn'
-    ]),
-    mounted: function() {
-      this.$store.dispatch('doFetchSignedIn')
-    },
-    methods: {
-      setError(error, text) {
-        this.error = (error.response && error.response.data && error.response.data.error) || text
-      },
-      signOut() {
-        this.$http.secured.delete(`/signin`)
-          .then(response => {
-            delete localStorage.csrf
-            delete localStorage.signedIn
-          })
-          .catch(error => this.setError(error, 'Cannot sign out'))
-      }
-    }
-  }
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+
 import {
   securedAxiosInstance,
   plainAxiosInstance
 } from '../javascript/axios/axios.js'
+
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 Vue.use(axios, {
@@ -17,6 +20,6 @@ Vue.use(axios, {
 new Vue({
   router,
   store,
-
+  vuetify,
   render: (h) => h(App)
 }).$mount('#app')
