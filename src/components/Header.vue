b6eaa5ac7c851c421f6502daae8df220fc6c3738
kotarofunyu <kotarofunyu@gmail.com>
2020-12-19 00:22:53 +0900

    [fix] メニューバーを広く

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,16 +13,25 @@
         <LoginModal />
       </v-app-bar>
       <v-navigation-drawer
-        app
-        floating
-        dark
-        color="#039BE5"
-        mini-variant
-        fixed
-        clipped
         v-model="drawer"
+        color="#039BE5"
+        :expand-on-hover="expandOnHover"
+        :mini-variant="miniVariant"
+        :permanent="permanent"
+        dark
+        app
       >
         <v-list>
+          <v-list-item two-line :class="miniVariant && 'px-0'">
+            <v-list-item-avatar>
+              <img src="https://randomuser.me/api/portraits/men/81.jpg">
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>User</v-list-item-title>
+              <v-list-item-subtitle>kotaro</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
           <v-list-item v-for="item in items" :key="item.title" :to="item.link">
             <v-list-item-icon>
               <v-icon>{{ item.icon }}</v-icon>
