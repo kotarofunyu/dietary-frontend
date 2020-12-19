0399630cffa13de952c9aed6ee627451a3849a64
kotarofunyu <kotarofunyu@gmail.com>
2020-12-19 21:34:58 +0900

    [clean]不要なものを削除

--- src/components/BaseDataIterator.vue
+++ src/components/BaseDataIterator.vue
@@ -1,13 +1,7 @@
 <template lang="">
   <div>
-     <div
-        v-for="item in weekly"
-        >
-          {{ item }}
-        </div>
-    <p @click="weekly()">aaa</p>
     <v-data-iterator
-        :items="items"
+        :items="weekly"
         :items-per-page.sync="itemsPerPage"
         :page="page"
         :search="search"
@@ -37,7 +31,7 @@
                 flat
                 solo-inverted
                 hide-details
-                :items="keys"
+                :items="weekKeys"
                 prepend-inner-icon="mdi-magnify"
                 label="Sort by"
               ></v-select>
@@ -71,7 +65,7 @@
           <v-row>
             <v-col
               v-for="(value, index) in weekly"
-              :key="item"
+              :key="index"
               cols="12"
               sm="6"
               md="4"
@@ -194,115 +188,12 @@ export default {
         "Calcium",
         "Iron",
       ],
-      items: [
-        {
-          name: "第一週 89.4kg",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87,
-          calcium: "14%",
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: "8%",
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337,
-          calcium: "6%",
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: "3%",
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: "7%",
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          sodium: 50,
-          calcium: "0%",
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          sodium: 38,
-          calcium: "0%",
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          sodium: 562,
-          calcium: "0%",
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          sodium: 326,
-          calcium: "2%",
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          sodium: 54,
-          calcium: "12%",
-          iron: "6%",
-        },
-      ],
       weekKeys: [1, 2, 3, 4, 5],
-      // weekly: []
     };
   },
   computed: {
     numberOfPages() {
-      return Math.ceil(this.items.length / this.itemsPerPage);
+      return Math.ceil(this.weekly.length / this.itemsPerPage);
     },
     filteredKeys() {
       return this.keys.filter((key) => key !== "Name");
