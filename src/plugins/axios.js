dbff40a35d902abd1cf2e8e922935b2f2a7183a8
kotarofunyu <kotarofunyu@gmail.com>
2021-01-07 21:56:48 +0900

    [fix]name.upperCase is not a functionが出るから直した（めっちゃうざかった）

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,11 +1,20 @@
 import axios from 'axios'
 import store from '@/store/index'
 
-axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT
-axios.defaults.headers = store.state.authToken
+// axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT
+// axios.defaults.headers = store.state.authToken
+
+// export default axios
+
+export default axios.create({
+  baseURL: process.env.VUE_APP_API_ENDPOINT,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: store.state.authToken
+  }
+})
+
 axios.interceptors.request.use(hoge => {
   console.log(hoge.url)
   return hoge
 })
-
-export default axios
