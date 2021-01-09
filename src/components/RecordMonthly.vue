44eabb6e1552fadf5b6a151645af83245ffc0436
kotarofunyu <kotarofunyu@gmail.com>
2021-01-09 20:38:08 +0900

    上記同様

--- src/components/RecordMonthly.vue
+++ src/components/RecordMonthly.vue
@@ -1,24 +1,63 @@
 <template lang="">
   <div>
     <v-row>
-      <LineChart :width="800" :height="500" />
+      <LineChart :data="chartData" :options="chartOptions" :width="800" :height="500" />
     </v-row>
     <v-row>
-      <RecordList />
+      <RecordList :weights="weights" />
     </v-row>
   </div>
 </template>
 <script>
-import LineChart from './Bar'
-import RecordList from './RecordList'
+import LineChart from "./Bar";
+import RecordList from "./RecordList";
 export default {
-  name: 'RecordMonthly',
+  name: "RecordAll",
   components: {
     LineChart,
-    RecordList
-  }
-}
+    RecordList,
+  },
+  computed: {
+    weights() {
+      return this.$store.state.weightsDatas;
+    },
+    chartData() {
+      const data = {
+        labels: this.$store.state.dates,
+        datasets: [
+          {
+            label: "データ",
+            data: this.$store.state.weights,
+          },
+        ],
+      };
+      return data;
+    },
+    chartOptions() {
+      const options = {
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Month",
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                stepSize: 5,
+                min: 85,
+              },
+            },
+          ],
+        },
+      };
+      return options;
+    },
+  },
+};
 </script>
 <style lang="">
-  
 </style>
