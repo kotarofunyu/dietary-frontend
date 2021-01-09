6b5c0abd1539aa7c8395d0ee4c71e13560f0592f
kotarofunyu <kotarofunyu@gmail.com>
2021-01-09 20:17:46 +0900

    fix

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,48 +1,52 @@
 <script>
-  import { Line } from 'vue-chartjs'
-  import axios from 'axios'
-  import { mapState } from 'vuex'
-  export default {
-    name: 'LineChart',
-    extends: Line,
-    data () {
-      return {
-        data: {
-          labels: [],
-          datasets: [
+import { Line } from "vue-chartjs";
+import axios from "axios";
+import { mapState } from "vuex";
+export default {
+  name: "LineChart",
+  extends: Line,
+  data() {
+    return {
+      data: {
+        labels: [],
+        datasets: [
+          {
+            lineTention: 0,
+            label: "体重",
+            data: [],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          xAxes: [
             {
-              lineTention: 0,
-              label: "体重",
-              data: []
-            }
-          ]
-        },
-        options: {
-          scales: {
-            xAxes: [{
               scaleLabel: {
                 display: true,
-                labelString: 'Month'
-              }
-            }],
-            yAxes: [{
+                labelString: "Month",
+              },
+            },
+          ],
+          yAxes: [
+            {
               ticks: {
                 beginAtZero: true,
                 stepSize: 0.5,
-                min: 85
-              }
-            }]
-          }
-        }
-      }
-    },
-    mounted () {
-      this.$store.dispatch('getWeightsDatas')
-      this.data.labels = this.$store.state.dates
-      this.data.datasets[0].data = this.$store.state.weights
-      this.renderChart(this.data, this.options)
-    }
-  }
+                min: 85,
+              },
+            },
+          ],
+        },
+      },
+    };
+  },
+  mounted() {
+    this.$store.dispatch("getWeightsDatas");
+    this.data.labels = this.$store.state.dates;
+    this.data.datasets[0].data = this.$store.state.weights;
+    this.renderChart(this.data, this.options);
+  },
+};
 </script>
 
 <style>
